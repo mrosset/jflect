c2162f8815240e60af1091622677fbd841d71aba
str1ngs <mike.rosset@gmail.com>
2012-03-30 11:19:00 -0700

    remove package flag, output json tags

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"unicode"
 )
 
 var (
-	client   = new(http.Client)
-	fstruct  = flag.String("s", "Foo", "struct name for json object")
-	fpackage = flag.String("p", "main", "package name")
-	furl     = flag.String("u", "", "url for json input")
+	client  = new(http.Client)
+	fstruct = flag.String("s", "User", "struct name for json object")
+	furl    = flag.String("u", "https://api.github.com/users/str1ngs", "url for json input")
 )
 
 func main() {
@@ -25,35 +25,58 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-	//var tjson = []byte(`{ "name": "Joe", "age": 25 }`)
 	var v interface{}
 	res, err := client.Get(*furl)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if res.StatusCode != http.StatusOK {
+		errf := fmt.Errorf("%s %v %s", *furl, res.StatusCode,
+			http.StatusText(res.StatusCode))
+		log.Fatal(errf)
+	}
 	err = json.NewDecoder(res.Body).Decode(&v)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err = Reflect(os.Stdout, v, *fpackage, *fstruct); err != nil {
+	if err = reflect(os.Stdout, v, *fstruct); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func Reflect(w io.Writer, i interface{}, pkg, strct string) (err error) {
+func reflect(w io.Writer, i interface{}, strct string) (err error) {
 	bb := new(bytes.Buffer)
 	switch i := i.(type) {
 	case map[string]interface{}:
-		fmt.Fprintf(bb, "package %s\n\n", pkg)
 		fmt.Fprintf(bb, "type %s struct {\n", strct)
 		for key, val := range i {
-			vstr := fmt.Sprintf("%T", val)
-			if vstr == "<nil>" {
-				vstr = "nil"
+			if len(key) == 0 {
+				return fmt.Errorf("len or map key is 0")
 			}
-			fmt.Fprintf(bb, "%s %s\n", key, vstr)
+			gotype := fmt.Sprintf("%T", val)
+			switch gotype {
+			case "<nil>":
+				gotype = "nil"
+			case "float64":
+				gotype = "int"
+			}
+			mkUpper := true
+			field := ""
+			for _, c := range key {
+				if mkUpper {
+					c = unicode.ToUpper(c)
+					mkUpper = false
+				}
+				if c == '_' {
+					mkUpper = true
+					continue
+				}
+				field += string(c)
+			}
+			fmt.Fprintf(bb, "%s %s `json:\"%s\"`\n", field, gotype, key)
 		}
 		fmt.Fprintln(bb, "}")
+
 		cmd := exec.Command("gofmt")
 		cmd.Stdin = bb
 		cmd.Stdout = w
@@ -61,7 +84,6 @@ func Reflect(w io.Writer, i interface{}, pkg, strct string) (err error) {
 		if err = cmd.Run(); err != nil {
 			return err
 		}
-
 	default:
 		return fmt.Errorf("unexpected type")
 	}
