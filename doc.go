7deb78707a4dff6db578c16612e5881ecee1a39b
mike rosset <mike.rosset@gmail.com>
2012-07-26 13:14:14 -0700

    doc: minor formatting fixes

--- doc.go
+++ doc.go
@@ -18,22 +18,22 @@ Web service:
 Output:
 
 type GithubUser struct {
-	AvatarUrl   string `json:"avatar_url"`
-	Bio         string `json:"bio"`
-	CreatedAt   string `json:"created_at"`
-	Followers   int    `json:"followers"`
-	Following   int    `json:"following"`
-	GravatarId  string `json:"gravatar_id"`
-	Hireable    bool   `json:"hireable"`
-	HtmlUrl     string `json:"html_url"`
-	Id          int    `json:"id"`
-	Location    string `json:"location"`
-	Login       string `json:"login"`
-	Name        string `json:"name"`
-	PublicGists int    `json:"public_gists"`
-	PublicRepos int    `json:"public_repos"`
-	Type        string `json:"type"`
-	Url         string `json:"url"`
+ AvatarUrl   string `json:"avatar_url"`
+ Bio         string `json:"bio"`
+ CreatedAt   string `json:"created_at"`
+ Followers   int    `json:"followers"`
+ Following   int    `json:"following"`
+ GravatarId  string `json:"gravatar_id"`
+ Hireable    bool   `json:"hireable"`
+ HtmlUrl     string `json:"html_url"`
+ Id          int    `json:"id"`
+ Location    string `json:"location"`
+ Login       string `json:"login"`
+ Name        string `json:"name"`
+ PublicGists int    `json:"public_gists"`
+ PublicRepos int    `json:"public_repos"`
+ Type        string `json:"type"`
+ Url         string `json:"url"`
 }
 
 From file:
@@ -44,14 +44,14 @@ From file:
 Saving is just a matter of redirecting stdout to a file.
 
 	jflect < foo.json > foo.go
-	curl https://api.github.com/users/str1ngs | jflect -s GihubUser > githuser.go
 
+	curl https://api.github.com/users/str1ngs | jflect -s GihubUser > gituser.go
 
 Notes
 
-Jflect is primarily designed to fast prototype go structs. Its not
-intended for automation. It does handle nested json objects. But does
-currently handle arbitrary json arrays
+Jflect is primarily designed to fast prototype go structs. Its not intended for 
+automation. It does handle nested json objects. But does not currently handle 
+arbitrary json arrays.
 
 */
 package documentation
